2b4dfb769487ad36003a836fa19d70e3c3ce3fca
Timothy Palko <tpalko@mac-topazapple.home>
2014-10-30 00:41:45 -0400

    added bash shell provisioning script

---
+++ provision.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "Beginning Provisioning Run"
+date
+
+apt-get update
+apt-get -y upgrade
+
+which java -version
+
+if [ ! $? -eq 0 ]
+then
+	
+	# - anyone see a problem here?
+	sudo apt-get -y install default-jdk
+
+	if ! grep --quiet "java-7-openjdk-amd64" ~/.bashrc
+	then		
+		echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bashrc
+	fi
+
+	export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+fi
+
+which mvn --version
+
+if [ ! $? -eq 0 ]
+then
+
+	if [ ! -d /usr/share/apache-maven-3.2.3 ]
+	then
+
+		if [ ! -f apache-maven-3.2.3-bin.tar.gz ]
+		then
+			wget http://www.trieuvan.com/apache/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
+		fi
+
+		tar -xf apache-maven-3.2.3-bin.tar.gz
+		cp -R apache-maven-3.2.3 /usr/share
+	fi
+
+	if ! grep --quiet "apache-maven" ~/.bashrc
+	then		
+		echo "export PATH=$PATH:/usr/share/apache-maven-3.2.3/bin" >> ~/.bashrc
+	fi
+
+	export PATH=$PATH:/usr/share/apache-maven-3.2.3/bin
+fi
+
+cd /vagrant
+mvn tomcat7:run
+
+echo "Provisioning Run Complete"
+date
